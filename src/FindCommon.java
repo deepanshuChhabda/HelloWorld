f3e46ae0967154ed0edbe08eb4274324ce9ebb71
de4shuch3da <deepanshuchhabda1998@gmail.com>
2019-01-06 11:19:42 +0530

    optimZATTION

--- src/FindCommon.java
+++ src/FindCommon.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 class FindCommon {
     public static void main(String[] args) {
-        int t = 0;
+        int t;
         Scanner in = new Scanner(System.in);
         t = in.nextInt();
         in.nextLine();
         try {
-        while (t-- > 0) {
-            String sen = in.nextLine();
-            int len = sen.length();
-            boolean k = false;
+            while (t-- > 0) {
+                String sen = in.nextLine();
+                int len = sen.length();
+                boolean k = false;
                 if (sen.charAt(0) == 'n' && sen.charAt(1) == 'o' && sen.charAt(2) == 't' && sen.charAt(3) == ' ')
                     System.out.println("Real Fancy");
                 else {
@@ -32,8 +32,8 @@ class FindCommon {
                     if (!k) System.out.println("regularly fancy");
                 }
             }
-        }catch (Exception e) {
-            return;
+        } catch (Exception e) {
+
         }
 
     }
